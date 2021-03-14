f80859944f3753f47d37046c65954cdf1b14051a
primat-lab <80504558+primat-lab@users.noreply.github.com>
2021-03-14 09:13:23 +0000

    Update reserford.py

--- src/reserford.py
+++ src/reserford.py
@@ -8,46 +8,56 @@ class Reserford:
     """
 
     def __init__(self):
-        self.alphaMass = 6.644
-        self.alphaEnergy = 8.0109
-        self.alphaCharge = 2 * 1.60217662
-        self.coreMass = 410
-        self.coreCharge = 79 * 1.60217662
+        self.alphaMass = 4.001506179125
+        self.alphaEnergy = 5
+        self.alphaCharge = 2 * 1.6021766208e-19
+        self.coreMass = 196.966569
+        self.coreCharge = 79 * 1.6021766208e-19
+        self.R = 1.66e-10
         self.alpha_x = 0
         self.alpha_y = 0
-        self.core_x = 1
-        self.core_y = 0.51
-        self.k = 0.05
-        self.time = 10
-        self.delta_t = 0.001
+        self.core_x = 1.66e-46
+        self.core_y = 1.66e-49
+        self.k = 1.380649e-23
+        self.time = 1e-53
+        self.delta_t = 1e-57
         self.val = []
 
     def calculate(self):
         """via Verlet integration"""
 
+        self.alphaMass *= 1.660538918685e-27
+        self.alphaEnergy *= 1.6021766e-13
+        self.coreMass *= 1.660538918685e-27
+
         def apply_forces_alpha():
             nonlocal pos_alpha, pos_core
             r12 = pos_alpha - pos_core
-            f = self.k * self.alphaCharge * self.coreCharge / np.dot(r12, r12) * r12 / np.sqrt(np.dot(r12, r12))
+            mod_r12 = np.sqrt(np.dot(r12, r12))
+            f = self.k * self.alphaCharge * self.coreCharge / np.dot(r12, r12) * r12 / mod_r12\
+                * (0, 1 - mod_r12 / self.R)[int(mod_r12 < self.R)]
             a = f / self.alphaMass
             return a
 
         def apply_forces_core():
             nonlocal pos_alpha, pos_core
             r12 = pos_core - pos_alpha
-            f = self.k * self.alphaCharge * self.coreCharge / np.dot(r12, r12) * r12 / np.sqrt(np.dot(r12, r12))
+            mod_r12 = np.sqrt(np.dot(r12, r12))
+            f = self.k * self.alphaCharge * self.coreCharge * self.R / np.dot(r12, r12) * r12 / mod_r12\
+                * (0, 1 - mod_r12 / self.R)[int(mod_r12 < self.R)]
             a = f / self.coreMass
             return a
 
         def update(dt):
             nonlocal pos_alpha, vel_alpha, acc_alpha, pos_core, vel_core, acc_core
 
-            new_pos_core = pos_core + vel_core * dt + acc_core * dt * dt / 2
             new_pos_alpha = pos_alpha + vel_alpha * dt + acc_alpha * dt * dt / 2
-            new_acc_core = apply_forces_core()
-            new_vel_core = vel_core + (acc_core + new_acc_core) * dt / 2
             new_acc_alpha = apply_forces_alpha()
             new_vel_alpha = vel_alpha + (acc_alpha + new_acc_alpha) * dt / 2
+
+            new_pos_core = pos_core + vel_core * dt + acc_core * dt * dt / 2
+            new_acc_core = apply_forces_core()
+            new_vel_core = vel_core + (acc_core + new_acc_core) * dt / 2
 
             pos_core = new_pos_core
             acc_core = new_acc_core
@@ -82,32 +92,57 @@ class Reserford:
         5. core acceleration hodograph
         """
         if __name__ == '__main__':
-            ax1 = plt.subplot(311)
-            ax2 = plt.subplot(323)
-            ax1.plot([row[0][0] for row in self.val], [row[0][1] for row in self.val])
-            ax1.plot([row[3][0] for row in self.val], [row[3][1] for row in self.val])
-            ax1.scatter([row[3][0] for row in self.val], [row[3][1] for row in self.val])
-            ax2.plot([row[1][0] for row in self.val], [row[1][1] for row in self.val])
-            ax3 = plt.subplot(324)
-            ax3.plot([row[4][0] for row in self.val], [row[4][1] for row in self.val])
-            ax4 = plt.subplot(325)
-            ax4.plot([row[2][0] for row in self.val], [row[2][1] for row in self.val])
-            ax5 = plt.subplot(326)
-            ax5.plot([row[5][0] for row in self.val], [row[5][1] for row in self.val])
+            pos_both_graph = plt.subplot(211)
+            pos_alpha_graph = plt.subplot(223)
+            pos_core_graph = plt.subplot(224)
+
+            pos_both_graph.set_xlabel('x (м)')
+            pos_both_graph.set_ylabel('y (м)')
+            pos_both_graph.set_title('Траектория обоих частиц')
+            pos_both_graph.grid()
+
+            pos_alpha_graph.set_xlabel('x (м)')
+            pos_alpha_graph.set_ylabel('y (м)')
+            pos_alpha_graph.set_title('Траектория альфа-частицы')
+            pos_alpha_graph.grid()
+
+            pos_core_graph.set_xlabel('x (м)')
+            pos_core_graph.set_ylabel('y (м)')
+            pos_core_graph.set_title('Траектория ядра атома золота')
+            pos_core_graph.grid()
+
+            pos_both_graph.plot([row[0][0] for row in self.val], [row[0][1] for row in self.val])
+            pos_both_graph.scatter([row[3][0] for row in self.val], [row[3][1] for row in self.val], marker='.')
+            pos_alpha_graph.plot([row[0][0] for row in self.val], [row[0][1] for row in self.val])
+            pos_core_graph.plot([row[3][0] for row in self.val], [row[3][1] for row in self.val])
+
             plt.show()
+
         else:
-            plt.scatter([row[3][0] for row in self.val], [row[3][1] for row in self.val])
-            plt.plot([row[0][0] for row in self.val], [row[0][1] for row in self.val])
-            plt.show()
-            plt.plot([row[3][0] for row in self.val], [row[3][1] for row in self.val])
-            plt.show()
-            plt.plot([row[1][0] for row in self.val], [row[1][1] for row in self.val])
-            plt.show()
-            plt.plot([row[4][0] for row in self.val], [row[4][1] for row in self.val])
-            plt.show()
-            plt.plot([row[2][0] for row in self.val], [row[2][1] for row in self.val])
-            plt.show()
-            plt.plot([row[5][0] for row in self.val], [row[5][1] for row in self.val])
+            pos_both_graph = plt.subplot(211)
+            pos_alpha_graph = plt.subplot(223)
+            pos_core_graph = plt.subplot(224)
+
+            pos_both_graph.set_xlabel('x (м)')
+            pos_both_graph.set_ylabel('y (м)')
+            pos_both_graph.set_title('Траектория обоих частиц')
+            pos_both_graph.grid()
+
+            pos_alpha_graph.set_xlabel('x (м)')
+            pos_alpha_graph.set_ylabel('y (м)')
+            pos_alpha_graph.set_title('Траектория альфа-частицы')
+            pos_alpha_graph.grid()
+
+            pos_core_graph.set_xlabel('x (м)')
+            pos_core_graph.set_ylabel('y (м)')
+            pos_core_graph.set_title('Траектория ядра атома золота')
+            pos_core_graph.grid()
+
+            pos_both_graph.plot([row[0][0] for row in self.val], [row[0][1] for row in self.val])
+            pos_both_graph.scatter([row[3][0] for row in self.val], [row[3][1] for row in self.val], marker='.')
+            pos_alpha_graph.plot([row[0][0] for row in self.val], [row[0][1] for row in self.val])
+            pos_core_graph.plot([row[3][0] for row in self.val], [row[3][1] for row in self.val])
+
             plt.show()
 
 
